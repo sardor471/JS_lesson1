2a5b9d1a8895dfb9d223809ae2720b1119d8870f
sardor471 <sardorIT45@gmail.com>
2026-02-04 21:07:52 +0500

    first commit

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
+// let x = 2;
+// let Y = 3*x**6-6*x**2-7;
+// console.log("Y Funksiyaning qiymati:", Y)
+
+
+
+
 // begin 1
-// let a = 5;
-// let b = 10;
-// let S = a*b;
-// let P = 2*(a+b)
-// console.log("To'g'ri to'rtburchak yuzasi:", S, "/n", "To'g'ri to'rtburchak perimetri:", P)
+let a = 5;
+let b = 10;
+let S = a*b;
+let P = 2*(a+b)
+console.log("To'g'ri to'rtburchak yuzasi:", S, "/n", "To'g'ri to'rtburchak perimetri:", P)
 
+// begin 2
+let d = 5;
+let pi = 3.14;
+let l = pi*d;
+let r = d%2;
+let s = pi*r**2;
+console.log("Aylana uzunligi:", l, "Aylana yuzasi:", S)
 
-let x = 2;
-let Y = 3*x**6-6*x**2-7;
-console.log("Y Funksiyaning qiymati:", Y)
+// begin 3
+let a1 = 10;
+let v = a1**3;
+let s1 = 6*a**2;
+console.log("Kubning xajmi:", v, "Kubning sirti:", s)
 
+// begin 4
+let a2 = 2;
+let d2 = 3;
+let c2 = 4;
+let v2 = a2*d2*c2;
+let s2 = 2*(a2*d2+d2*c2+a2*c2)
+console.log("Paralelipipedning hajmi:", v2, "/N", "Paralelipipedning tola sirti:", c2)
+
+// begin 5
+let a3 = 5;
+let b3 = 6;
+let m = (a+b)%2;
+console.log("a va b ning orta arifmetigii:", m)
