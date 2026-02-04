553d978206cd26840ae612c56b3e175526ca2ed2
sallaberdiyev462-bit <sallaberdiyev462@gmail.com>
2026-02-04 17:17:46 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// begin 1
+// let a = 5;
+// let b = 10;
+// let S = a*b;
+// let P = 2*(a+b)
+// console.log("To'g'ri to'rtburchak yuzasi:", S, "/n", "To'g'ri to'rtburchak perimetri:", P)
+
+
+let x = 2;
+let Y = 3*x**6-6*x**2-7;
+console.log("Y Funksiyaning qiymati:", Y)
+
