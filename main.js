acd3a62ad8faaf7dad3fb99ecdd5959050683604
sardor471 <sardorIT45@gmail.com>
2026-02-06 16:43:08 +0500

    first commit

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ console.log("To'g'ri to'rtburchak yuzasi:", S, "/n", "To'g'ri to'rtburchak perim
 let d = 5;
 let pi = 3.14;
 let l = pi*d;
-let r = d%2;
+let r = d/2;
 let s = pi*r**2;
 console.log("Aylana uzunligi:", l, "Aylana yuzasi:", S)
 
@@ -37,5 +37,86 @@ console.log("Paralelipipedning hajmi:", v2, "/N", "Paralelipipedning tola sirti:
 // begin 5
 let a3 = 5;
 let b3 = 6;
-let m = (a+b)%2;
+let m = (a+b)/2;
 console.log("a va b ning orta arifmetigii:", m)
+
+// begin 6
+let a4 = 4
+let b4 = 5
+let c = (a4**2 + b4**2) ** 1/2
+let p = a4 + b4 + c
+console.log("Uchburchakning gipotenuzasi:", c, "Uchburchakning perimetri:", p)
+
+// begin 7
+let r1 = 25
+let r2 = 20
+let c1 = pi+r1
+let c3 =  pi+r2
+let s3 = pi*(r1**2 - r2**2)
+console.log("Aylana yuzalari ayirmasi:", s3)
+
+// begin 8
+let r3 = 6
+let l1 = 2*pi*r3
+let s4 = pi*r3**2
+console.log("Aylana radiusi:", l1, "Aylana yuzasi:", s4)
+
+// begin 9
+let x1 = 15
+let x2 = 10
+let y1 = 8
+let y2 = 6
+let l2 = ((x1 - x2)**2 + (y1 - y2)**2) ** 1/2
+console.log("Tekislikdagi ikki nuqtaning masofasi:", l2)
+
+// begin 10
+let a5 = 10;
+let b5 = 20;
+let c4 = 30;
+[a5, b5] = [b5, a5];
+[b5, c4] = [c4, b5];
+[c4, a5] = [a5, c4];
+console.log(a5, b5, c4)
+
+// begin 11
+let x3 = 7;
+let y3 = 4*(x3-3)**6-7*(x3-3)**3+2;
+console.log("Y funksiyaning qiymati:", y3)
+
+// begin 12
+let a6 = 2;
+console.log("A ning darajalari:", a6**2, a6**6, a6**9, a6**15)
+
+// begin 13
+let tf = 60;
+let tc = (tf-32)*5%9
+console.log(tc)
+
+// begin 14
+let x4 = 10;
+let a7 = 40;
+let y4 = 10;
+let b7 = 30;
+let m1 = (a7/x4)-(b7/y4);
+console.log(m1) 
+
+// begin 15
+let x5 = 4;
+let a8 = 6;
+let y5 = 6;
+let b8 = 8;
+let m2 = x5*a8 + y5*b8;
+console.log("Abdulaziz jami qilgan savdosi:", m2)
+
+// integer 1
+let number = 946;
+let birlik = number%10;
+let onlik = parseInt(number/10)%10;
+console.log(birlik, onlik)
+
+// integer 2
+let N = 356
+let Nbirlik = N%10;
+let Nonlik = parseInt(N/10)%10;
+let Nyuzlik = parseInt(N/100);
+console.log(Nbirlik, Nonlik ,Nyuzlik, Nbirlik + Nonlik + Nyuzlik)
